d869896cd6409b21e5dae3dccdd475dcdf146364
dongrishiren <1760204951@qq.com>
2017-05-11 14:01:17 +0800

    初期项目

--- business/settings.py
+++ business/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'business.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sport_business',
+        'USER': 'admin',
+        'PASSWORD': 'szq814211077',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
